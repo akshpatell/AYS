feb0b9d783d269652c9dc0ec2a2b6ba086c16d97
aksh <akshpatel08032003@gmail.com>
2023-09-04 09:42:51 +0530

    fifth page upgrad

--- builder/src/components/Fifthpage.jsx
+++ builder/src/components/Fifthpage.jsx
@@ -6,7 +6,7 @@ import { AiOutlineLeft } from "react-icons/ai";
 const Fifthpage = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-full py-14 max-sm:py-0">
+      <div className="flex flex-col justify-center items-center h-full py-14 max-sm:py-5">
         <p className="text-5xl max-sm:text-3xl max-lg:text-4xl p-3">Add-ons</p>
 
         <div className="border-2 flex flex-col rounded mt-3 w-[50vw] max-lg:w-[90vw] gap-5 pt-5 max-md:w-[90vw] max-sm:w-[96vw] max-sm:gap-5 max-sm:pt-2 max-sm:my-2">
